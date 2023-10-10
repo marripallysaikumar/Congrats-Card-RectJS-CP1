6e87aa6f04551f27d659708225ee89e1a436768f
saikumarmarripally <saikumarmarripally38@gmail.com>
2023-10-10 08:13:33 +0000

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="background-image">
+    <h1>Congrulations</h1>
+    <div className="sub-container">
+      <img
+        src="https://res.cloudinary.com/dwj82i4ec/image/upload/v1696924361/ram-sita-floral-ribbon-qjxh3b7rzn9c8ubf_zyekqh.webp"
+        className="img-size"
+      />
+      <h3>Jai sri Rama</h3>
+      <p>sita rama sita rama sita rama</p>
+      <img
+        src="https://res.cloudinary.com/dwj82i4ec/image/upload/v1696924851/lord-rama-minimalist-art-9lbadwx401p6tkjq_kamqbo.webp"
+        className="img-size"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
